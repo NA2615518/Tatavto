d0e02fcf722bc264188def8e46ba09c4d5b31de9
Evgenii Gavrilin <globallgef@gmail.com>
2022-05-24 15:33:56 +0300

    update index and model.js  Fix main.css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
 
 import {model} from ' ./model' // импортируем в model.js //
 import { title, text, columns, image} from './templates'
+import ' ./main.css'
 
 const $site = document.querySelector( '#site')
 
@@ -23,42 +24,3 @@ model.forEach(block  => {
     $site.insertAdjacentHTML(where: 'beforeend', html)
 })
 
-function title(block) {
-    return `
-    <div class="row">
-    <div class="col-sm">
-    <h1>${block.value}</h1>
-    </div>
-    </div>
-    `
-}
-
-function text(block) {
-    return `
-    <div class="row">
-    <div class="col-sm">
-    <p>${block.value}</p>
-    </div>
-    </div>
-    `
-}
-
-function columns(block) {
-    const html = block.value.map(item => `<div class="col-sm">${item}</div> `)
-    return `
-    <div class="row">
-    ${html.join ('') }
-    </div>
-    `
-}
-
-function  image(block) {
-    return `
-    <div class="row">
-    <img src="${block.value}" />
-    </div>
-    `
-
-    
-    
-}
